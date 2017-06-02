a35ff619643e64c474223e40d3ed0f2c3086415f
Olaoluwa Osuntokun <laolu32@gmail.com>
2017-06-02 16:20:16 -0700

    filterdb: create new package for storing downloaded filter
    This commit creates a new package: `filterdb` which is dedicated to
    storing the downloaded filter headers from network peers. An interface
    has been erected in front of the first concrete implementation in order
    to make unit testing the core of neutrino easier in the future.

---
+++ filterdb/db.go
@@ -0,0 +1,203 @@
+package filterdb
+
+import (
+	"fmt"
+
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcutil/gcs"
+	"github.com/btcsuite/btcutil/gcs/builder"
+	"github.com/btcsuite/btcwallet/walletdb"
+)
+
+var (
+	// filterBucket is the name of the root bucket for this package. Within
+	// this bucket, sub-buckets are stored which themselves store the
+	// actual filters.
+	filterBucket = []byte("filter-store")
+
+	// regBucket is the bucket that stores the regular filters.
+	regBucket = []byte("regular")
+
+	// extBucket is the name of the bucket that stores the extended
+	// filters.
+	extBucket = []byte("ext")
+)
+
+// FilterType is a enum-like type that represents the various filter types
+// currently defined.
+type FilterType uint8
+
+const (
+	// RegularFilter is the filter type of regular filters which contain
+	// outputs and pkScript data pushes.
+	RegularFilter = iota
+
+	// ExtendedFilter is the filter type of extended filters which contain
+	// txids, and witness data.
+	ExtendedFilter
+)
+
+var (
+	// ErrFilterNotFound is returned when a filter for a target block hash is
+	// unable to be located.
+	ErrFilterNotFound = fmt.Errorf("unable to find filter")
+)
+
+// FilterDatabase is an interface which represents an object that is capable of
+// storing and retiring filters according to their corresponding block hash and
+// also their filter type.
+//
+// TODO(roasbeef): similar interface for headerfs?
+type FilterDatabase interface {
+	// PutFilter stores a filter with the given hash and type to persistent
+	// storage.
+	PutFilter(*chainhash.Hash, *gcs.Filter, FilterType) error
+
+	// FetchFilter attempts to fetch a filter with the given hash and type
+	// from persistent storage. In the case that a filter matching the
+	// target block hash cannot be found, then ErrFilterNotFound is to be
+	// returned.
+	FetchFilter(*chainhash.Hash, FilterType) (*gcs.Filter, error)
+}
+
+// FilterStore is an implementation of the FilterDatabase interface which is
+// backed by boltdb.
+type FilterStore struct {
+	db walletdb.DB
+
+	chainParams chaincfg.Params
+}
+
+// A compile-time check to ensure the FilterStore adheres to the FilterDatabase
+// interface.
+var _ FilterDatabase = (*FilterStore)(nil)
+
+// New creates a new instance of the FilterStore given an already open
+// database, and the target chain parameters.
+func New(db walletdb.DB, params chaincfg.Params) (*FilterStore, error) {
+	err := walletdb.Update(db, func(tx walletdb.ReadWriteTx) error {
+		// As part of our initial setup, we'll try to create the top
+		// level filter bucket. If this already exists, then we can
+		// exit early.
+		filters, err := tx.CreateTopLevelBucket(filterBucket)
+		if err != nil {
+			return err
+		}
+
+		// If the main bucket doesn't already exist, then we'll need to
+		// create the sub-buckets, and also initialize them with the
+		// genesis filters.
+		genesisBlock := params.GenesisBlock
+		genesisHash := params.GenesisHash
+
+		// First we'll create the bucket for the regular filters.
+		regFilters, err := filters.CreateBucketIfNotExists(regBucket)
+		if err != nil {
+			return err
+		}
+
+		// With the bucket created, we'll now construct the initial
+		// basic genesis filter and store it within the database.
+		basicFilter, err := builder.BuildBasicFilter(genesisBlock)
+		if err != nil {
+			return err
+		}
+		err = putFilter(regFilters, genesisHash, basicFilter)
+		if err != nil {
+			return err
+		}
+
+		// Similarly, we'll then create the bucket to store the
+		// extended filters, build the genesis tiler and finally store
+		// it within the database.
+		extFilters, err := filters.CreateBucketIfNotExists(extBucket)
+		if err != nil {
+			return err
+		}
+
+		extFilter, err := builder.BuildExtFilter(genesisBlock)
+		if err != nil {
+			return err
+		}
+		return putFilter(extFilters, genesisHash, extFilter)
+	})
+	if err != nil && err != walletdb.ErrBucketExists {
+		return nil, err
+	}
+
+	return &FilterStore{
+		db: db,
+	}, nil
+}
+
+// putFilter stores a filter in the database according to the corresponding
+// block hash. The passed bucket is expected to be the proper bucket for the
+// passed filter type.
+func putFilter(bucket walletdb.ReadWriteBucket, hash *chainhash.Hash,
+	filter *gcs.Filter) error {
+
+	return bucket.Put(hash[:], filter.NBytes())
+}
+
+// PutFilter stores a filter with the given hash and type to persistent
+// storage.
+//
+// NOTE: This method is a part of the FilterDatabase interface.
+func (f *FilterStore) PutFilter(hash *chainhash.Hash,
+	filter *gcs.Filter, fType FilterType) error {
+
+	return walletdb.Update(f.db, func(tx walletdb.ReadWriteTx) error {
+		filters := tx.ReadWriteBucket(filterBucket)
+
+		var targetBucket walletdb.ReadWriteBucket
+		switch fType {
+		case RegularFilter:
+			targetBucket = filters.NestedReadWriteBucket(regBucket)
+		case ExtendedFilter:
+			targetBucket = filters.NestedReadWriteBucket(extBucket)
+		}
+
+		return targetBucket.Put(hash[:], filter.NBytes())
+	})
+}
+
+// FetchFilter attempts to fetch a filter with the given hash and type from
+// persistent storage.
+//
+// NOTE: This method is a part of the FilterDatabase interface.
+func (f *FilterStore) FetchFilter(blockHash *chainhash.Hash,
+	filterType FilterType) (*gcs.Filter, error) {
+
+	var filter *gcs.Filter
+
+	err := walletdb.View(f.db, func(tx walletdb.ReadTx) error {
+		filters := tx.ReadBucket(filterBucket)
+
+		var targetBucket walletdb.ReadBucket
+		switch filterType {
+		case RegularFilter:
+			targetBucket = filters.NestedReadBucket(regBucket)
+		case ExtendedFilter:
+			targetBucket = filters.NestedReadBucket(extBucket)
+		}
+
+		filterBytes := targetBucket.Get(blockHash[:])
+		if filterBytes == nil {
+			return ErrFilterNotFound
+		}
+
+		dbFilter, err := gcs.FromNBytes(builder.DefaultP, filterBytes)
+		if err != nil {
+			return err
+		}
+
+		filter = dbFilter
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return filter, nil
+}
---
+++ filterdb/db_test.go
@@ -0,0 +1,154 @@
+package filterdb
+
+import (
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcutil/gcs"
+	"github.com/btcsuite/btcwallet/walletdb"
+	_ "github.com/btcsuite/btcwallet/walletdb/bdb"
+)
+
+func createTestDatabase() (func(), FilterDatabase, error) {
+	tempDir, err := ioutil.TempDir("", "neutrino")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	db, err := walletdb.Create("bdb", tempDir+"/test.db")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	cleanUp := func() {
+		os.RemoveAll(tempDir)
+		db.Close()
+	}
+
+	filterDB, err := New(db, chaincfg.SimNetParams)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return cleanUp, filterDB, nil
+}
+
+func TestGenesisFilterCreation(t *testing.T) {
+	cleanUp, database, err := createTestDatabase()
+	defer cleanUp()
+	if err != nil {
+		t.Fatalf("unable to create test db: %v", err)
+	}
+
+	genesisHash := chaincfg.SimNetParams.GenesisHash
+
+	// With the database initialized, we should be able to fetch both the
+	// regular and extended filter for the genesis block.
+	regGenesisFilter, err := database.FetchFilter(genesisHash, RegularFilter)
+	if err != nil {
+		t.Fatalf("unable to fetch regular genesis filter: %v", err)
+	}
+
+	// The regular filter should be non-nil as the gensis block's output
+	// should be indexed.
+	if regGenesisFilter == nil {
+		t.Fatalf("regular genesis filter is nil")
+	}
+
+	extGenesisFilter, err := database.FetchFilter(genesisHash, ExtendedFilter)
+	if err != nil {
+		t.Fatalf("unable to fetch ext genesis fitler: %v", err)
+	}
+
+	// The extended filter should also be non-nil as the the genesis
+	// filter's transaction should be indexed.
+	if extGenesisFilter == nil {
+		t.Fatalf("regular genesis filter is nil")
+	}
+}
+
+func genRandFilter(numElements uint32) (*gcs.Filter, error) {
+	elements := make([][]byte, numElements)
+	for i := uint32(0); i < numElements; i++ {
+		var elem [20]byte
+		if _, err := rand.Read(elem[:]); err != nil {
+			return nil, err
+		}
+
+		elements[i] = elem[:]
+	}
+
+	var key [16]byte
+	if _, err := rand.Read(key[:]); err != nil {
+		return nil, err
+	}
+
+	filter, err := gcs.BuildGCSFilter(20, key, elements)
+	if err != nil {
+		return nil, err
+	}
+
+	return filter, nil
+}
+
+func TestFilterStorage(t *testing.T) {
+	// TODO(roasbeef): use testing.Quick
+	cleanUp, database, err := createTestDatabase()
+	defer cleanUp()
+	if err != nil {
+		t.Fatalf("unable to create test db: %v", err)
+	}
+
+	// We'll generate a random block hash to create our test filters
+	// against.
+	var randHash chainhash.Hash
+	if _, err := rand.Read(randHash[:]); err != nil {
+		t.Fatalf("unable to generate random hash: %v", err)
+	}
+
+	// First, we'll create and store a random fitler for the regular filter
+	// type for the block hash generate above.
+	regFilter, err := genRandFilter(100)
+	if err != nil {
+		t.Fatalf("unable to create random filter: %v", err)
+	}
+	err = database.PutFilter(&randHash, regFilter, RegularFilter)
+	if err != nil {
+		t.Fatalf("unable to store regular filter: %v", err)
+	}
+
+	// Next, we'll do the same for the exteended fitler type.
+	extFilter, err := genRandFilter(100)
+	if err != nil {
+		t.Fatalf("unable to create random filter: %v", err)
+	}
+	err = database.PutFilter(&randHash, extFilter, ExtendedFilter)
+	if err != nil {
+		t.Fatalf("unable to store extended filter: %v", err)
+	}
+
+	// With the filter stored, we should be able to retrieve the filter
+	// without any issue, and it should match the stored filter exactly.
+	regFilterDB, err := database.FetchFilter(&randHash, RegularFilter)
+	if err != nil {
+		t.Fatalf("unable to retrieve reg filter: %v", err)
+	}
+	if !reflect.DeepEqual(regFilter, regFilterDB) {
+		t.Fatalf("regular filter doesn't match!")
+	}
+
+	// Similarly, we should also be able to retrive the extended fitler,
+	// and it should match what we initially put in.
+	extFitlerDB, err := database.FetchFilter(&randHash, ExtendedFilter)
+	if err != nil {
+		t.Fatalf("unable to retrieve ext filter: %v", err)
+	}
+	if !reflect.DeepEqual(extFilter, extFitlerDB) {
+		t.Fatalf("extended filter doesn't match!")
+	}
+}
