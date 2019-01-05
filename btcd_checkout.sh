a20375365e18ea54717ebf49128054872cccbef5
Loshan T <loshan1212@gmail.com>
2019-01-05 12:45:39 +0000

    ltc: update refs

--- btcd_checkout.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-git checkout $(cat go.sum | grep -A1 github.com/btcsuite/btcd | tail -n1 | awk -F " " '{ print $2 }' | awk -F "-" '{ print $3 }' | awk -F "/" '{ print $1 }')
---
+++ ltcd_checkout.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git checkout $(cat go.sum | grep -A1 github.com/ltcsuite/ltcd | tail -n1 | awk -F " " '{ print $2 }' | awk -F "-" '{ print $3 }' | awk -F "/" '{ print $1 }')
