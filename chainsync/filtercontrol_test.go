97eea55cd8e82749ce4d673afbabbecd0ddbbd8a
Johan T. Halseth <johanth@gmail.com>
2019-06-18 15:34:04 +0200

    chainsync: define ControlCFHeader
    ControlCFHeader controls the passed filter header against a list of
    hardcoded checkpoints, that we can use to determine peers serving
    invalid filterheaders.

---
+++ chainsync/filtercontrol_test.go
@@ -0,0 +1,52 @@
+package chainsync
+
+import (
+	"testing"
+
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/wire"
+)
+
+func TestControlCFHeader(t *testing.T) {
+	t.Parallel()
+
+	// We'll modify our backing list of checkpoints for this test.
+	height := uint32(999)
+	header := hashFromStr(
+		"4a242283a406a7c089f671bb8df7671e5d5e9ba577cea1047d30a7f4919df193",
+	)
+	filterHeaderCheckpoints = map[wire.BitcoinNet]map[uint32]*chainhash.Hash{
+		chaincfg.MainNetParams.Net: map[uint32]*chainhash.Hash{
+			height: header,
+		},
+	}
+
+	// Expect the control at height to succeed.
+	err := ControlCFHeader(
+		chaincfg.MainNetParams, wire.GCSFilterRegular, height, header,
+	)
+	if err != nil {
+		t.Fatalf("error checking height: %v", err)
+	}
+
+	// Pass an invalid header, this should return an error.
+	header = hashFromStr(
+		"000000000006a7c089f671bb8df7671e5d5e9ba577cea1047d30a7f4919df193",
+	)
+	err = ControlCFHeader(
+		chaincfg.MainNetParams, wire.GCSFilterRegular, height, header,
+	)
+	if err != ErrCheckpointMismatch {
+		t.Fatalf("expected ErrCheckpointMismatch, got %v", err)
+	}
+
+	// Finally, control an unknown height. This should also pass since we
+	// don't have the checkpoint stored.
+	err = ControlCFHeader(
+		chaincfg.MainNetParams, wire.GCSFilterRegular, 99, header,
+	)
+	if err != nil {
+		t.Fatalf("error checking height: %v", err)
+	}
+}
