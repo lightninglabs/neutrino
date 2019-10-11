c9779c09f170dbc79d5b19edf26280436ae7221b
Johan T. Halseth <johanth@gmail.com>
2019-10-11 11:02:43 +0200

    travis: fix btcd checkout by moving into script
    Previously the expected version was not checked out. Now we use go get
    to fetch the correct module version.

--- btcd_checkout.sh
+++ btcd_checkout.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-git pull && git checkout $(cat go.sum | grep -A1 github.com/btcsuite/btcd | tail -n1 | awk -F " " '{ print $2 }' | awk -F "-" '{ print $3 }' | awk -F "/" '{ print $1 }')
+BTCD_COMMIT=$(cat go.mod | \
+        grep github.com/btcsuite/btcd | \
+        tail -n1 | \
+        awk -F " " '{ print $2 }' | \
+        awk -F "/" '{ print $1 }')
+echo "Fetching btcd at $BTCD_COMMIT"
+
+pushd /tmp
+GO111MODULE=on go get -v github.com/btcsuite/btcd@$BTCD_COMMIT
+popd
