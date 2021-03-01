42923a1880ea42d5042947d9126c63e0396c88ea
Oliver Gugger <gugger@gmail.com>
2021-03-01 14:06:34 +0100

    headerfs: add baseline benchmarks
    As a preparation for memory behavior optimizations, we first add a set
    of benchmark tests to establish a baseline against.
    The benchmarks determine the speed and memory usage of writing different
    sized batches to the bbolt index as well as the random access latency
    for retrieving entries from the index.

--- headerfs/index_test.go
+++ headerfs/index_test.go
@@ -3,7 +3,9 @@ package headerfs
 import (
 	"bytes"
 	"crypto/rand"
+	"fmt"
 	"io/ioutil"
+	mathRand "math/rand"
 	"os"
 	"testing"
 	"time"
@@ -26,8 +28,10 @@ func createTestIndex() (func(), *headerIndex, error) {
 	}
 
 	cleanUp := func() {
-		os.RemoveAll(tempDir)
-		db.Close()
+		_ = db.Close()
+		fi, _ := os.Stat(tempDir + "/test.db")
+		fmt.Printf("DB file size at cleanup: %d bytes\n", fi.Size())
+		_ = os.RemoveAll(tempDir)
 	}
 
 	filterDB, err := newHeaderIndex(db, Block)
@@ -48,23 +52,9 @@ func TestAddHeadersIndexRetrieve(t *testing.T) {
 	// First, we'll create a a series of random headers that we'll use to
 	// write into the database.
 	const numHeaders = 100
-	headerEntries := make(headerBatch, numHeaders)
-	headerIndex := make(map[uint32]headerEntry)
-	for i := uint32(0); i < numHeaders; i++ {
-		var header headerEntry
-		if _, err := rand.Read(header.hash[:]); err != nil {
-			t.Fatalf("unable to read header: %v", err)
-		}
-		header.height = i
-
-		headerEntries[i] = header
-		headerIndex[i] = header
-	}
-
-	// With the headers constructed, we'll write them to disk in a single
-	// batch.
-	if err := hIndex.addHeaders(headerEntries); err != nil {
-		t.Fatalf("unable to add headers: %v", err)
+	headerEntries, headerIndex, err := writeRandomBatch(hIndex, numHeaders)
+	if err != nil {
+		t.Fatalf("error writing random batch: %v", err)
 	}
 
 	// Next, verify that the database tip matches the _final_ header
@@ -118,4 +108,134 @@ func TestAddHeadersIndexRetrieve(t *testing.T) {
 		t.Fatalf("tip doesn't match: expected %x, got %x",
 			lastEntry.hash[:], dbTip[:])
 	}
+}
+
+// BenchmarkWriteHeadersSmallBatch measures the performance of writing 500k
+// headers to the database in small size batches (100 headers per batch).
+func BenchmarkWriteHeadersSmallBatch(b *testing.B) {
+	const (
+		batchSize  = 100
+		numBatches = 5000
+	)
+	for n := 0; n < b.N; n++ {
+		cleanUp, hIndex, err := createTestIndex()
+		if err != nil {
+			b.Fatalf("unable to create test db: %v", err)
+		}
+
+		for j := 0; j < numBatches; j++ {
+			_, _, err := writeRandomBatch(hIndex, batchSize)
+			if err != nil {
+				b.Fatalf("error writing random batch: %v", err)
+			}
+		}
+
+		cleanUp()
+	}
+}
+
+// BenchmarkWriteHeadersMediumBatch measures the performance of writing 500k
+// headers to the database in medium size batches (2000 headers per batch).
+func BenchmarkWriteHeadersMediumBatch(b *testing.B) {
+	const (
+		batchSize  = 2000
+		numBatches = 250
+	)
+	for n := 0; n < b.N; n++ {
+		cleanUp, hIndex, err := createTestIndex()
+		if err != nil {
+			b.Fatalf("unable to create test db: %v", err)
+		}
+
+		for j := 0; j < numBatches; j++ {
+			_, _, err := writeRandomBatch(hIndex, batchSize)
+			if err != nil {
+				b.Fatalf("error writing random batch: %v", err)
+			}
+		}
+
+		cleanUp()
+	}
+}
+
+// BenchmarkWriteHeadersLargeBatch measures the performance of writing 500k
+// headers to the database in large size batches (10000 headers per batch).
+func BenchmarkWriteHeadersLargeBatch(b *testing.B) {
+	const (
+		batchSize  = 10000
+		numBatches = 50
+	)
+	for n := 0; n < b.N; n++ {
+		cleanUp, hIndex, err := createTestIndex()
+		if err != nil {
+			b.Fatalf("unable to create test db: %v", err)
+		}
+
+		for j := 0; j < numBatches; j++ {
+			_, _, err := writeRandomBatch(hIndex, batchSize)
+			if err != nil {
+				b.Fatalf("error writing random batch: %v", err)
+			}
+		}
+
+		cleanUp()
+	}
+}
+
+// BenchmarkHeightLookupLatency benchmarks the speed for randomly accessing the
+// index with a hash.
+func BenchmarkHeightLookupLatency(b *testing.B) {
+	// Start by creating an index with 10k headers.
+	cleanUp, hIndex, err := createTestIndex()
+	if err != nil {
+		b.Fatalf("unable to create test db: %v", err)
+	}
+
+	const batchSize = 50000
+	_, headerIndex, err := writeRandomBatch(hIndex, batchSize)
+	if err != nil {
+		b.Fatalf("error writing random batch: %v", err)
+	}
+
+	// Only start the benchmark counter now, for this test we don't want to
+	// know how fast we can write the index but how fast we can read it.
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		index := uint32(mathRand.Int31n(batchSize))
+		hash := headerIndex[index].hash
+		_, err := hIndex.heightFromHash(&hash)
+		if err != nil {
+			b.Fatalf("error fetching height: %v", err)
+		}
+	}
+
+	cleanUp()
+}
+
+// writeRandomBatch creates a random batch with numHeaders headers and writes it
+// to the given header index in one update transaction.
+func writeRandomBatch(hIndex *headerIndex, numHeaders uint32) (headerBatch,
+	map[uint32]headerEntry, error) {
+
+	headerEntries := make(headerBatch, numHeaders)
+	headerIndex := make(map[uint32]headerEntry)
+	for i := uint32(0); i < numHeaders; i++ {
+		var header headerEntry
+		if _, err := rand.Read(header.hash[:]); err != nil {
+			return nil, nil, fmt.Errorf("unable to read header: %v",
+				err)
+		}
+		header.height = i
+
+		headerEntries[i] = header
+		headerIndex[i] = header
+	}
+
+	// With the headers constructed, we'll write them to disk in a single
+	// batch.
+	if err := hIndex.addHeaders(headerEntries); err != nil {
+		return nil, nil, fmt.Errorf("unable to add headers: %v", err)
+	}
+
+	return headerEntries, headerIndex, nil
 }
