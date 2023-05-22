f5637cc40ca4d7a0d3d063c0d4c91e33a9cac423
Elle Mouton <elle.mouton@gmail.com>
2023-05-22 15:53:49 +0200

    query+blockmanager: add NumRetries optional param
    In this commit, the worker manager is adjusted to only retry a request
    a certain maximum number of times. This is done so that a smooth
    transition can be made from call sights currently using the
    neutrino.queryChainServicePeers method to using the query.WorkManager
    instead so that the same default behaviour can be expected. In order to
    also cater for call sights currently using the query.WorkManager, a
    NoRetryMax optional paramater is added that can used to disable the
    retry maximum.

--- query/interface.go
+++ query/interface.go
@@ -14,6 +14,10 @@ const (
 	// defaultQueryEncoding specifies the default encoding (witness or not)
 	// for `getdata` and other similar messages.
 	defaultQueryEncoding = wire.WitnessEncoding
+
+	// defaultNumRetries is the default number of times that a query job
+	// will be retried.
+	defaultNumRetries = 2
 )
 
 // queries are a set of options that can be modified per-query, unlike global
@@ -30,6 +34,15 @@ type queryOptions struct {
 	// cancelChan is an optional channel that can be closed to indicate
 	// that the query should be canceled.
 	cancelChan chan struct{}
+
+	// numRetries is the number of times that a query should be retried
+	// before failing.
+	numRetries uint8
+
+	// noRetryMax is set if no cap should be applied to the number of times
+	// that a query can be retried. If this is set then numRetries has no
+	// effect.
+	noRetryMax bool
 }
 
 // QueryOption is a functional option argument to any of the network query
@@ -43,7 +56,7 @@ func defaultQueryOptions() *queryOptions {
 	return &queryOptions{
 		timeout:    defaultQueryTimeout,
 		encoding:   defaultQueryEncoding,
-		cancelChan: nil,
+		numRetries: defaultNumRetries,
 	}
 }
 
@@ -51,6 +64,22 @@ func defaultQueryOptions() *queryOptions {
 func (qo *queryOptions) applyQueryOptions(options ...QueryOption) {
 	for _, option := range options {
 		option(qo)
+	}
+}
+
+// NumRetries is a query option that specifies the number of times a query
+// should be retried.
+func NumRetries(num uint8) QueryOption {
+	return func(qo *queryOptions) {
+		qo.numRetries = num
+	}
+}
+
+// NoRetryMax is a query option that can be used to disable the cap on the
+// number of retries. If this is set then NumRetries has no effect.
+func NoRetryMax() QueryOption {
+	return func(qo *queryOptions) {
+		qo.noRetryMax = true
 	}
 }
 
