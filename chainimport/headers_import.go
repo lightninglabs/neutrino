ad995274fed409844c33750bd8640d2fe3957a20
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-08-12 07:33:41 +0000

    chainimport: import only on empty target stores

--- chainimport/headers_import.go
+++ chainimport/headers_import.go
@@ -2,6 +2,7 @@ package chainimport
 
 import (
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/btcsuite/btcd/chaincfg"
@@ -29,12 +30,56 @@ func NewHeadersImport(options *ImportOptions) (*headersImport, error) {
 	return importer, nil
 }
 
-// Import import headers data in target header stores.
+// Import import headers data in target header stores. The Import process is
+// currently performed only if the target stores are completely empty except for
+// gensis block/filter header otherwise it is entirely skipped. On first
+// development iteration, it is designed to serve new users who don't yet have
+// headers data, or existing users who are willing to reset their headers data.
 func (h *headersImport) Import() (*ImportResult, error) {
+	isFresh, err := h.isTargetFresh(
+		h.options.TargetBlockHeaderStore,
+		h.options.TargetFilterHeaderStore,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to detect if target stores "+
+			"are fresh import failed: %w", err)
+	}
+	if !isFresh {
+		log.Info("Skipping headers import: target header stores are " +
+			"not empty")
+		return &ImportResult{}, nil
+	}
+
 	return &ImportResult{}, nil
 }
 
-// ImportOptions defines options for the import process.
+// isTargetFresh checks if the target header stores are in their initial state,
+// meaning they contain only the genesis header (height 0).
+func (h *headersImport) isTargetFresh(
+	targetBlockHeaderStore headerfs.BlockHeaderStore,
+	targetFilterHeaderStore headerfs.FilterHeaderStore) (bool, error) {
+
+	// Get the chain tip from both target stores.
+	_, blockTipHeight, err := targetBlockHeaderStore.ChainTip()
+	if err != nil {
+		return false, fmt.Errorf("failed to get target block header "+
+			"chain tip: %w", err)
+	}
+
+	_, filterTipHeight, err := targetFilterHeaderStore.ChainTip()
+	if err != nil {
+		return false, fmt.Errorf("failed to get target filter header "+
+			"chain tip: %w", err)
+	}
+
+	if blockTipHeight == 0 && filterTipHeight == 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+// ImportOptions defines parameters for the import process.
 type ImportOptions struct {
 	// TargetChainParams specifies the blockchain network parameters for the
 	// chain into which headers will be imported.
--- chainimport/headers_import_test.go
+++ chainimport/headers_import_test.go
@@ -1,9 +1,13 @@
 package chainimport
 
 import (
+	"errors"
 	"testing"
 
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/wire"
 	_ "github.com/btcsuite/btcwallet/walletdb/bdb"
+	"github.com/lightninglabs/neutrino/headerfs"
 	"github.com/stretchr/testify/require"
 )
 
@@ -48,6 +52,100 @@ func TestHeadersConjunctionProperty(t *testing.T) {
 				return
 			}
 			require.NoError(t, err)
+		})
+	}
+}
+
+// TestTargetStoreFreshnessDetection tests the logic for detecting whether the
+// target header stores are fresh (only contain genesis headers). It covers
+// various combinations of block and filter header heights and error cases.
+func TestTargetStoreFreshnessDetection(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		name                 string
+		blockHeight          uint32
+		filterHeight         uint32
+		expectFresh          bool
+		expectErr            bool
+		expectFilterStoreErr error
+		expectBlockStoreErr  error
+	}{
+		{
+			name:         "OnlyGenesisHeadersExist",
+			blockHeight:  0,
+			filterHeight: 0,
+			expectFresh:  true,
+		},
+		{
+			name:         "BlockHeightGreaterThanZero",
+			blockHeight:  1,
+			filterHeight: 0,
+			expectFresh:  false,
+		},
+		{
+			name:         "FilterHeightGreaterThanZero",
+			blockHeight:  0,
+			filterHeight: 1,
+			expectFresh:  false,
+		},
+		{
+			name:         "BothHeightsGreaterThanZero",
+			blockHeight:  10,
+			filterHeight: 10,
+			expectFresh:  false,
+		},
+		{
+			name:      "ErrorOnBlockStore",
+			expectErr: true,
+			expectBlockStoreErr: errors.New(
+				"failed to get target block header",
+			),
+		},
+		{
+			name:      "ErrorOnFilterStore",
+			expectErr: true,
+			expectFilterStoreErr: errors.New(
+				"failed to get target filter header",
+			),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			mockBlockStore := &headerfs.MockBlockHeaderStore{}
+			mockBlockStore.On("ChainTip").Return(
+				&wire.BlockHeader{}, tc.blockHeight,
+				tc.expectBlockStoreErr,
+			)
+
+			mockFilterStore := &headerfs.MockFilterHeaderStore{}
+			mockFilterStore.On("ChainTip").Return(
+				&chainhash.Hash{}, tc.filterHeight,
+				tc.expectFilterStoreErr,
+			)
+
+			importer := &headersImport{}
+
+			isFresh, err := importer.isTargetFresh(
+				mockBlockStore, mockFilterStore,
+			)
+			if tc.expectErr {
+				if tc.expectBlockStoreErr != nil {
+					require.ErrorContains(
+						t, err,
+						tc.expectBlockStoreErr.Error(),
+					)
+				}
+				if tc.expectFilterStoreErr != nil {
+					require.ErrorContains(
+						t, err,
+						tc.expectFilterStoreErr.Error(),
+					)
+				}
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, isFresh, tc.expectFresh)
 		})
 	}
 }
